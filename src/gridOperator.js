ac668070df42a5021ea55d1dcadba7d719d62ad4
canerdikkollu <cdikkollu@gmail.com>
2019-01-27 17:02:10 +0300

    v0- first commit

---
+++ src/gridOperator.js
@@ -0,0 +1,68 @@
+var queries = decodeURIComponent(window.location.search);
+queries = queries.substring(1);
+var username = queries.split("=");
+console.log(username[1]);
+document.getElementById("grdOpName").value = username[1];
+document.getElementById("grdOpName").disabled = true;
+
+const contractAddress = "0x76bae75af2f5e7e550f9dd41faa00cd4d64a8b78";
+const abi = [{ "constant": true, "inputs": [{ "name": "_ownerType", "type": "uint256" }], "name": "getMinEnergyPriceAccordingToOwnerType", "outputs": [{ "name": "", "type": "string" }, { "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "productInfoStruct", "outputs": [{ "name": "ownerName", "type": "string" }, { "name": "energyPrice", "type": "uint256" }, { "name": "ownerType", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "startIndex", "type": "uint256" }, { "name": "wantedOwnerType", "type": "uint256" }], "name": "wantedValueofProductInfoStruct", "outputs": [{ "name": "", "type": "string" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_ownerType", "type": "uint256" }], "name": "getAnOwnerLength", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "ownerName", "type": "string" }, { "name": "energyPrice", "type": "uint256" }, { "name": "_ownerType", "type": "uint256" }, { "name": "profitRate", "type": "uint256" }], "name": "addOffer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
+let contract = web3.eth.contract(abi).at(contractAddress);
+window.addEventListener('load', () => {
+    if (typeof (web3) == "undefined") {
+        console.log("Metamask is not installed...");
+    } else {
+        console.log("Metamask is installed...");
+    }
+    /* Use getMinEnergyPriceAccordingToOwnerType funtion of smart contract to get best value of all
+    offers. In this page ownerType parametre is o. Because, Grid operators need Energy Producers's offers. */
+    contract.getMinEnergyPriceAccordingToOwnerType.call(0, (error, result) => {
+        if (!error) {
+            $("#station").html('Best Price is ' + result[1] + '$. from ' + result[0]);//Give output to user.
+            console.log(result);
+        } else
+            console.error(error);
+    });
+    getAllOffers();
+
+    function getAllOffers() {
+        var lengtha;
+        contract.getAnOwnerLength.call(0,
+            (error, result) => {
+                if (error) {
+                    return console.log(error);
+                }
+                lengtha = result;
+                var i;
+                var index = 0;// First starting index is 0
+                for (i = 0; i < lengtha; i++) {
+                    contract.wantedValueofProductInfoStruct(index, 0, (error, result) => {
+                        if (error) {
+                            return console.log(error);
+                        }
+                        var table = document.getElementById("myTable");
+                        var row = table.insertRow(1);
+                        var cell1 = row.insertCell(0);
+                        var cell2 = row.insertCell(1);
+                        cell1.innerHTML = result[0];//Add Table
+                        cell2.innerHTML = result[1];//Add Table
+                        index = result[2];// New index is return value of smart contract function.
+                    });//Smart contract function. Find the next Energy Producer.
+                }
+                //console.log(length);
+            });
+        console.log(lengtha);
+    }
+});
+function setOffer() {
+    //Use addOffer function of deployed smart contract in web3 Provider. Send parametres to add new offer.
+    contract.addOffer.sendTransaction($("#grdOpName").val(), $("#grdOpPrice").val(), 1, $("#grdOpPrfRate").val(),
+        { from: web3.eth.accounts[0], gas: 3000000 },
+        (error, result) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log("txHash= " + result);
+        }
+    );
+}
---
+++ src/login.js
@@ -0,0 +1,60 @@
+
+const contract_address = "0x1cb709f50e65ea6c334831e8b4aff5e7dfc42a32";
+const abi = [{"constant":true,"inputs":[{"name":"_userName","type":"string"},{"name":"_userPassworde","type":"string"}],"name":"checkUserLogin","outputs":[{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_userWalletAdress","type":"string"},{"name":"_userName","type":"string"},{"name":"_userPassworde","type":"string"},{"name":"_userType","type":"string"}],"name":"userRegister","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"userInfoStruct","outputs":[{"name":"userWalletAdress","type":"string"},{"name":"userName","type":"string"},{"name":"userPassword","type":"string"},{"name":"userType","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+let contract;
+window.addEventListener('load', () => {
+    if (typeof (web3) === 'undefined') {
+        return console.log("Metamask is not installed");
+    }
+    contract = web3.eth.contract(abi).at(contract_address);
+
+});
+
+web3.eth.getAccounts(function (error, result) {
+    if (error != null)
+        console.log("Couldn't get accounts");
+    console.log(result[0]);
+
+    web3.eth.getBalance(result[0], (err, balance) => {
+        balance = this.web3.fromWei(balance, "ether") + " ETH"
+        console.log(balance);
+    });
+});
+
+function validationcheck() {
+    let username = $('#field-username').val();
+    let password = $('#field-psw').val();
+    contract.checkUserLogin.sendTransaction(username, password, (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        else {
+            console.log("txhash: " + result);
+            contract.checkUserLogin.call(username, password, (error, result) => {
+                if (error) {
+                    return console.log(error);
+                }
+                if (result[1] == true) {
+                    var actionName = result[0] + ".html";
+                    if (actionName != "") {
+                        console.log(actionName);
+                        document.getElementById("form_id").action = actionName;
+                        document.getElementById("form_id").submit();
+                    } else {
+                        alert("Please set form action");
+                    }
+                }
+                else {
+                    // If status value is false, return error.
+                    console.log(result[0]);
+                }
+            });
+        }
+    });
+
+}
+
+
+
+
